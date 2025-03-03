7dba2be95e574eec12016c431d37b09489b4ab23
Kristina <kristinashrestha2055@gmail.com>
2025-03-03 14:47:00 -0500

    Adding footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer">
+        <p>© 2025 Real Estate. All Rights Reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
